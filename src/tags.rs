996afe7e852f2ce8ce4317274861cd552e4d1a54
Evan Purkhiser <evanpurkhiser@gmail.com>
2025-10-13 12:44:58 -0400

    Add MediaHash to Id3TagId

--- src/tags.rs
+++ src/tags.rs
@@ -15,6 +15,7 @@ pub enum Id3TagId {
     Genre,
     Track,
     Disc,
+    MediaHash,
 }
 
 impl Id3TagId {
@@ -22,11 +23,17 @@ impl Id3TagId {
     pub fn read(&self, tag: &Tag) -> Option<String> {
         tag.get(self.as_str())
             .and_then(|f| match self {
+                // CatalogId is stored as a comment
                 Id3TagId::CatlogId => f
                     .content()
                     .comment()
-                    .map(|c| c.text.as_str().trim_matches(char::from(0))),
-                _ => f.content().text(),
+                    .map(|c| c.text.as_str().trim_matches(char::from(0)).to_string()),
+                // The mediahash is stored as binary data in the UFID frame
+                Id3TagId::MediaHash => f
+                    .content()
+                    .unique_file_identifier()
+                    .map(|ufid| hex::encode(&ufid.identifier)),
+                _ => f.content().text().map(str::to_string),
             })
             .map(|v| v.to_string())
             .filter(|v| !v.is_empty())
@@ -46,6 +53,7 @@ impl Id3TagId {
             Self::Genre => "TCON",
             Self::Track => "TRCK",
             Self::Disc => "TPOS",
+            Self::MediaHash => "UFID",
         }
     }
 }
